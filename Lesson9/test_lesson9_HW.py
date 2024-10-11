a86695e62e3e73bb9ddff8a1b53d48d57c1280ec
OlgaSkyPro <olga60@rambler.ru>
2024-10-11 19:14:16 +0300

    HW9 after check

--- Lesson9/test_lesson9_HW.py
+++ Lesson9/test_lesson9_HW.py
@@ -19,20 +19,31 @@ def test_insert():
     db.execute('''INSERT into subject (subject_id, subject_title) VALUES(17, 'Algebra');''')
     rows = db.execute("select * from subject").fetchall()
     row1=rows[-1]
+    sql = text("DELETE FROM subject WHERE subject_id = :id")
+    db.execute(sql, id=17)
     assert row1["subject_title"] == 'Algebra'
-    
 
 def test_update():
      db = create_engine(db_connection_string)
-     sql = text("update subject set subject_title = :new_title where subject_id=:id")
-     db.execute(sql, new_title = 'Matan', id = 17)
+     db.execute('''INSERT into subject (subject_id, subject_title) VALUES(17, 'Algebra');''')
      rows = db.execute("select * from subject").fetchall()
      row1=rows[-1]
-     assert row1["subject_title"] == "Matan"
+     sql = text("update subject set subject_title = :new_title where subject_id=:id")
+     db.execute(sql, new_title = 'Matan', id = row1["subject_id"])
+     rows = db.execute("select * from subject").fetchall()
+     row2=rows[-1]
+     sql = text("DELETE FROM subject WHERE subject_id = :id")
+     id_update = row2["subject_id"]
+     db.execute(sql, id=id_update)
+     assert row2["subject_title"] == "Matan"
 
 
 def test_delete():
     db = create_engine(db_connection_string)
+    db.execute('''INSERT into subject (subject_id, subject_title) VALUES(17, 'Algebra');''')
+    rows = db.execute("select * from subject").fetchall()
+    row1=rows[-1]
+
     sql = text("DELETE FROM subject WHERE subject_id = :id")
     db.execute(sql, id=17)
     
